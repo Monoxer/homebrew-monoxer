e3424b4be1f9ebe459b07338e11df5a9f0452d86
Kaito Udagawa <kaito.udagawa@monoxer.com>
2022-03-22 22:16:09 +0900

    Update monoxer-adoptopenjdk8.rb

--- Casks/monoxer-adoptopenjdk8.rb
+++ Casks/monoxer-adoptopenjdk8.rb
@@ -1,8 +1,8 @@
 cask "monoxer-adoptopenjdk8" do
-  version "8,292,b10"
+  version "8,292,10"
   sha256 "4e200bc752337abc9dbfddf125db6a600f2ec53566f6f119a83036c8242a7672"
 
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.csv[0]}-binaries/releases/download/jdk#{version.csv[0]}u#{version.csv[1]}-#{version.csv[2]}/OpenJDK#{version.csv[0]}U-jdk_x64_mac_hotspot_#{version.csv[0]}u#{version.csv[1]}#{version.csv[2]}.pkg",
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.csv[0]}-binaries/releases/download/jdk#{version.csv[0]}u#{version.csv[1]}-b#{version.csv[2]}/OpenJDK#{version.csv[0]}U-jdk_x64_mac_hotspot_#{version.csv[0]}u#{version.csv[1]}b#{version.csv[2]}.pkg",
       verified: "https://github.com/AdoptOpenJDK"
   name "AdoptOpenJDK 8"
   desc "AdoptOpenJDK OpenJDK (Java) Development Kit"
@@ -11,7 +11,7 @@ cask "monoxer-adoptopenjdk8" do
   livecheck do
     url "https://github.com/adoptopenjdk/openjdk#{version.csv[0]}-binaries/releases/latest"
     strategy :page_match do |page|
-      page.scan(/href=.*jdk(#{version.csv[0]})u(#{version.csv[1]})-(#{version.csv[2]})/i)
+      page.scan(/href=.*jdk(#{version.csv[0]})u(#{version.csv[1]})-b(#{version.csv[2]})/i)
           .map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
     end
   end
