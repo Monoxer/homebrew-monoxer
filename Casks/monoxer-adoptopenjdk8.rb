9ff31fee5af195a2c62a6e16970328775eded60d
Kaito Udagawa <kaito.udagawa@monoxer.com>
2022-03-22 14:35:09 +0900

    Update monoxer-adoptopenjdk8.rb

--- Casks/monoxer-adoptopenjdk8.rb
+++ Casks/monoxer-adoptopenjdk8.rb
@@ -1,11 +1,16 @@
 cask "monoxer-adoptopenjdk8" do
-  version "8,292:b10"
+  version "8.292,b10"
   sha256 "4e200bc752337abc9dbfddf125db6a600f2ec53566f6f119a83036c8242a7672"
 
-  # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.pkg",
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.pkg",
       verified: "https://github.com/AdoptOpenJDK"
-  appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases/latest"
+  livecheck do
+    url "https://github.com/adoptopenjdk/openjdk#{version.major}-binaries/releases/latest"
+    strategy :page_match do |page|
+      page.scan(/href=.*jdk(#{version.major})u(#{version.minor})-(#{version.csv.second})/i)
+          .map { |match| "#{match[0]}.#{match[1]},#{match[2]}" }
+    end
+  end
   name "AdoptOpenJDK 8"
   desc "AdoptOpenJDK OpenJDK (Java) Development Kit"
   homepage "https://adoptopenjdk.net/"
