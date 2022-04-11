e36e05435196876bfbb64025433b394806427b71
Kaito Udagawa <umireon@gmail.com>
2022-04-11 16:25:57 +0900

    Update monoxer-cocoapods.rb

--- Formula/monoxer-cocoapods.rb
+++ Formula/monoxer-cocoapods.rb
@@ -7,8 +7,9 @@ class MonoxerCocoapods < Formula
 
   bottle do
     root_url "https://github.com/Monoxer/homebrew-monoxer/releases/download/monoxer-cocoapods-1.11.0"
-    sha256                               big_sur:      "45be56e366c91030d3e4e0bc30d77cb987c15b1bfe0eb2e861017a73c050c13b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "70cfd9d41b1eebab1bea0d951a4bb46c544c25631962adbcd6d9b75a299fc602"
+    sha256 cellar: :any,                 arm64_monterey: "dc1686550da5838227cc47f4d25c78c214a818c8f8f9e1d8eaca77ec676b6635"
+    sha256                               big_sur:        "45be56e366c91030d3e4e0bc30d77cb987c15b1bfe0eb2e861017a73c050c13b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70cfd9d41b1eebab1bea0d951a4bb46c544c25631962adbcd6d9b75a299fc602"
   end
 
   depends_on "pkg-config" => :build
