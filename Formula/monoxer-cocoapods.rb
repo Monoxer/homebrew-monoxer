385a8b04e8a58d4b0b09d9767b0a9e55f50c38ad
Kaito Udagawa <umireon@gmail.com>
2022-04-11 16:03:26 +0900

    monoxer-cocoapods 1.11.0

---
+++ Formula/monoxer-cocoapods.rb
@@ -0,0 +1,30 @@
+class MonoxerCocoapods < Formula
+  desc "Dependency manager for Cocoa projects"
+  homepage "https://cocoapods.org/"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.11.0.tar.gz"
+  sha256 "4f494e7651cdf1a7afae6117fb1ed33c919471d7bc3b7575a68d5c316faf567c"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "ruby" if Hardware::CPU.arm?
+
+  uses_from_macos "libffi", since: :catalina
+  uses_from_macos "ruby", since: :catalina
+
+  def install
+    if MacOS.version >= :mojave && MacOS::CLT.installed?
+      ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
+    end
+
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "cocoapods.gemspec"
+    system "gem", "install", "cocoapods-#{version}.gem"
+    # Other executables don't work currently.
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/pod", "list"
+  end
+end
