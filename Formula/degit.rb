e3f52c20c3c9595a121ead2bbb26c2f90da1e8a2
Kaito Udagawa <umireon@gmail.com>
2022-05-20 07:20:54 +0000

    degit: add 2.8.4 bottle.

--- Formula/degit.rb
+++ Formula/degit.rb
@@ -7,6 +7,12 @@ class Degit < Formula
   sha256 "2fa329afe68038c4c4d1542908503a4a09211d97f326b30a1c7422a8238203de"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Monoxer/homebrew-monoxer/releases/download/degit-2.8.4"
+    sha256 cellar: :any_skip_relocation, big_sur:      "09e05e2aa425a265a3864d5d89df6c11d66d9e13d58fb3139fb6b4edd7b9ed10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "43d388200d5dac6c7e7a0921bc595d6acbef57490da60dd1088bdaa677b12189"
+  end
+
   depends_on "node"
 
   def install
