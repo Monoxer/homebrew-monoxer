52d90c805314473f3b005206dd95ce60fa79245b
Kaito Udagawa <umireon@gmail.com>
2022-10-05 03:32:38 +0000

    monoxer-rome: add 0.24.1.66 bottle.

--- Formula/monoxer-rome.rb
+++ Formula/monoxer-rome.rb
@@ -5,6 +5,12 @@ class MonoxerRome < Formula
   sha256 "9ba615d1001ce6ce93cacf5e3b193d30f6aa25fa33014438105747785b71dc36"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Monoxer/homebrew-monoxer/releases/download/monoxer-rome-0.24.1.66"
+    sha256 cellar: :any_skip_relocation, big_sur:      "a946535817c7de87ade679660eea4bc3343c9532376f6ba2f0ed14a018777bf7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2f147c1ba8fd5d0944a1bc51c15a5982796892cfb71a84ba2683f3eccf58ded3"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc@8.10" => :build
 
